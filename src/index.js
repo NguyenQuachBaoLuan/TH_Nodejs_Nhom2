10691ba6b81034394859087f5a917cf2f86fddd1
NguyenQuachBaoLuan <nqbluan2000092@student.ctuet.edu.vn>
2023-09-19 08:51:11 +0700

    second commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express') 
-
+const ejs = require('ejs')
 const app = express()
 const port = 3000 
+app.set('view engine','ejs')
+app.set('views','./src/views')
 app.get('/', (req, res) => {  res.send('Hello World!') })
-app.get('/about', (req, res) => {  res.send('About!') }) 
+app.get('/about', (req, res) => {  res.render('about') }) 
+app.get('/test', (req, res) => {  res.render('test') }) 
+
 app.listen(port, () => { 
  console.log(`Example app listening on port ${port}`) })
